10fbdf6cb36e480db3ae537709b262a849cfff9e
Srujana-solivar <srujana@vendus.com>
2015-09-14 13:17:21 +0530

    added user microposts

---
+++ spec/factories.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+	factory :user do |user|
+		user.name					"Michael Hartl"
+		user.email					"mhartl@example.com"
+		user.password 				"foobar"
+		user.password_confirmation 	"foobar"
+	end
+end
+
+FactoryGirl.define do
+  sequence :email do |n|
+	"person-#{n}@example.com"
+  end
+end
+
+FactoryGirl.define do
+	factory :micropost do |micropost|
+		micropost.content "Foo bar"
+		micropost.association :user
+	end
+end
